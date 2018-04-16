a93e7fc185937e952691bf6103c3f6b2bde1926a
Demon-Wang <568654196@qq.com>
2018-04-16 15:12:09 +0800

    new web files

---
+++ web/logout.php
@@ -0,0 +1,22 @@
+<?php
+  //正确的注销session方法：
+  //1开启session
+  session_start();
+  if(isset($_SESSION['name'])){
+
+    
+  //2、清空session信息
+  $_SESSION = array();
+    
+  //3、清楚客户端sessionid
+  if(isset($_COOKIE[session_name()]))
+  {
+    setCookie(session_name(),'',time()-3600,'/');
+  }
+  //4、彻底销毁session
+  session_destroy();
+header("Location:login.php");
+  }
+  else
+  header("Location:login.php");
+?>
